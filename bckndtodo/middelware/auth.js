d514f7ff7947c3f23f0f1a0b2f629aa89f704321
Birendrajha <63343940+Birendrajha@users.noreply.github.com>
2020-12-11 12:25:42 +0530

    verify token

--- bckndtodo/middelware/auth.js
+++ bckndtodo/middelware/auth.js
@@ -4,7 +4,7 @@ const jwt =require('jsonwebtoken')
 const auth =async(req,res,next)=>{
     try{
     const token = req.headers.authorization.split(" ")[1];
-    const decoded =jwt.verify(token,'thisismynewcourse')
+    const decoded =jwt.verify(token,'mylovelifeistrash')
     req.userData=decoded;
     next()
 }catch(e){
